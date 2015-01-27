353e48a35c70c1ab5d4a341ef8d62f085c40941c
chez8251 <chi8251@yahoo.com>
2015-01-27 16:44:31 +0800

    Prompt Name Question

--- sample.js
+++ sample.js
@@ -1,3 +1,7 @@
-function popup (message) {
-        alert(message);
-      }
+function myFunction() {
+    var person = prompt("Please enter your name", "Enter Your Name Here");
+    if (person != null) {
+        document.getElementById("demo").innerHTML =
+        "Hello " + person + "! How are you today?";
+    }
+}
