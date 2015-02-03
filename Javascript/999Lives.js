5e7f290d5fb4efb60478245922da6a5c73579835
chez8251 <chi8251@yahoo.com>
2015-02-03 18:07:40 +0800

    999lives

---
+++ Javascript/999Lives.js
@@ -0,0 +1,4 @@
+for (var lives = 3; lives > 0; lives--) {
+  alert("You have " + lives + " lives left!");
+}
+alert("Game Over")
--- sample.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    var person = prompt("Please enter your name", "Enter Your Name Here");
-    if (person != null) {
-        document.getElementById("demo").innerHTML =
-        "Hello " + person + "! You must die today!";
-    }
-}
